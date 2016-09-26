0276b5c6724d07993c1250cdb2655fb360f99d86
Miroslav <bartos.store@gmail.com>
2016-09-26 13:36:43 +0200

    First REST api call.

--- app/app.js
+++ app/app.js
@@ -18,18 +18,68 @@ var ProfilePic = React.createClass({
 });
 
 var ProfileLink = React.createClass({
+	getInitialState: function() {
+		console.log("getInitialState");
+
+		return {
+			rest_data: ""	
+		};		
+	},
+
+	componentDidMount: function() {
+		console.log("componentDidMount");
+
+		this.serverRequest = $.get("https://jsonplaceholder.typicode.com/posts", function(result) {
+			var first_object = result[0];
+
+			console.log("first_object: " + first_object.title);
+			this.setState({
+				rest_data: first_object
+			});
+		}.bind(this));
+
+		console.log("end of componentDidMount");
+	},
+
+	componentWillUnmount: function() {
+		console.log("componentWillMount");
+
+		this.serverRequest.abort();
+	},
+
 	render: function() {
+		console.log("render: " + this.state.rest_data);
+		
 		return (
 			<div className="user-link">
 				<a href={'https://github.com/' + this.props.username}>
 					Github profile
 				</a>
+				<br />
+
+				{this.state.rest_data.title}
 			</div>
 		)
 	}
 });
 
 var ProfileName = React.createClass({
+	getInitialState: function() {
+		return {
+			hausy_rules: ""
+		};
+	},
+
+	componentDidMount: function() {
+		this.serverRequest = $.get("http://mydomain.cz:8080/SmartHome-1.0/api/rules", function(result) {
+			console.log(result);
+			console.log("test: " + result.evaluationNodeTitle);
+			console.log("test: " + result[0]);
+			console.log("test: " + result[1]);
+			console.log("test: " + result[0].evaluationNodeTitle);
+		}.bind(this));
+	},
+
 	render: function() {
 		return (
 			<div className="user-name">{this.props.name}</div>
